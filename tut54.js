c1c010c84005ac7e02d32f8dd2f455975f44f884
bk408 <bhavyakhatri1912@gmail.com>
2022-11-21 19:12:23 +0530

    first commit

---
+++ tut54.js
@@ -0,0 +1,45 @@
+
+// Alert in in-browser javascript 
+
+// let a = alert("This is a message");     // Alert does not return anything
+// console.log(a);
+
+// alert("This is a message");
+
+
+// Prompt in Javascript
+
+// let name = prompt("what is your name?" , "Guest");
+// console.log(name);
+
+
+// Confirm in Javascript
+
+ //let deletepost = confirm("Do you really want to delete this post?");
+
+ // Code to delete the post
+ //if(deletepost){
+  //        console.log("Your Post has been deleted successfully");
+// }
+
+ // code to cancel deletion of the post
+// else{
+ //   console.log("Your Post has not been deleted");
+ //}
+ 
+ 
+ //console.log(deletepost);
+
+
+ let age = prompt("Enter your age");
+ 
+ if (age>18) {
+    console.log("You are allowed in the Club");
+    
+ } 
+ else {
+    console.log("You are not allowed in this club");
+ }
+
+
+
